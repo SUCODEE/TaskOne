ad52c9a656c15028286fe3528364bae553a8e515
sucodee <sucodeins@gmail.com>
2024-06-20 13:40:33 +0200

    Initial Commit

---
+++ TheTaskOne/TheTaskOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheTaskOneApp.swift
+//  TheTaskOne
+//
+//  Created by  yourexz on 2024-06-20.
+//
+
+import SwiftUI
+
+@main
+struct TheTaskOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
